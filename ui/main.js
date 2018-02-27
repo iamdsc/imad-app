fa249e96ca6a879897cd496da06608cde366f93f
iamdsc <dilpreetchawla382@gmail.com>
2018-02-27 19:55:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-// alert('Hi I am Javascript!');
+//Counter Code
+var btn = document.getElementById("counter");
+var counter = 0;
+
+btn.onclick = function () {
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+};
