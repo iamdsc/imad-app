ed33330b7241168507d98b08e7166aa9d0756477
iamdsc <dilpreetchawla382@gmail.com>
2018-02-27 22:33:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ function createTemplate(data){
             </div>
         </body>
     </html>
-    `
+    `;
     return htmlTemplate;
 }
 
@@ -83,9 +83,9 @@ app.get('/ui/main.js', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function (req, res) {
-  counter += 1    
+  counter += 1   ; 
   res.send(counter.toString());    
-})
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
