c944da45b1c3706e7dfdd846a9d8ed5e4d514176
iamdsc <dilpreetchawla382@gmail.com>
2018-02-27 22:55:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,20 +20,36 @@ btn.onclick = function () {
 };
 
 //Submit name
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
-    //Make a request to the server and send the name
+    //Create a request object
+    var request = new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+            //Capture a list of names and render it as a list
+            var names = request.responseText;
+            names = JSON.parse(names);//Converting back to array
+            var list = '';
+            for(var i = 0;i<names.length;i++){
+            list += '<li>'+names[i] + '</li>';
+            }
+            var ul = document.getElementById("namelist");
+            ul.innerHTML = list;
+            }
+        }
+        
+    };
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
+    //Make the request
+    request.open('GET','http://dilpreetchawla382.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+
     
-    //Capture a list of names and render it as a list
-    var names=['name1','name2','name3','name4'];
-    var list = '';
-    for(var i = 0;i<names.length;i++){
-        list += '<li>'+names[i] + '</li>';
-    }
-    var ul = document.getElementById("namelist");
-    ul.innerHTML = list;
 };
 
 
