f4737292c09afa88364c63d3a652178916010f47
iamdsc <dilpreetchawla382@gmail.com>
2018-02-27 22:44:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,9 +100,9 @@ app.get('/:articleName', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function(req, res){
+app.get('/submit-name', function(req, res){ //URL /submit-name?name=....
     //Get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     //JSON - Javascript Object Notation - it is a way of converting JS objects to string.  
